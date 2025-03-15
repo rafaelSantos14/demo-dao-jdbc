1af0c7e422d4d4c0e8096c5bbbb6b68e87ace38d
Rafael Santos <dossantos_rafael@hotmail.com>
2025-03-15 11:54:29 -0300

    seller findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,11 +9,11 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		
-		Department obj = new Department(1, "TI");
-		Seller seller = new Seller(21, "Rafa", "rafa@gmail.com", new Date(), 3000.0, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,12 +2,13 @@
 
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	//Somente para não precisar expor a implementação direto, deixar somente a interface
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
